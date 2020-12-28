7934516ec3dd68b93b1287fd61f9af1fdd695b7c
Vlad <Vlad@KOMP>
2020-12-28 21:15:12 +0200

    Cover AuthorController with unit tests.

--- WhatWasRead_Core_React_Redux.NUnitTest/AuthorControllerTest.cs
+++ WhatWasRead_Core_React_Redux.NUnitTest/AuthorControllerTest.cs
@@ -1,7 +1,9 @@
-﻿using ASP.NET_Core_WhatWasRead.App_Data;
-using ASP.NET_Core_WhatWasRead.App_Data.DBModels;
-using ASP.NET_Core_WhatWasRead.Controllers;
+﻿using ASP.NET_Core_React_Redux_WhatWasRead.App_Data;
+using ASP.NET_Core_React_Redux_WhatWasRead.App_Data.DBModels;
+using ASP.NET_Core_React_Redux_WhatWasRead.Controllers;
+using ASP.NET_Core_React_Redux_WhatWasRead.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NSubstitute;
 using NUnit.Framework;
 using System;
@@ -9,6 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WhatWasRead_Core_React_Redux.NUnitTest.Helpers;
 
 namespace My_Progress_UnitTests
 {
@@ -16,242 +19,216 @@ namespace My_Progress_UnitTests
    public class AuthorControllerTest
    {
       private Author[] _authors = new Author[5]
-{
-         new Author {AuthorId = 1, FirstName = "F1", LastName="L1"},
-         new Author {AuthorId = 2, FirstName = "F2", LastName="L2"},
-         new Author {AuthorId = 3, FirstName = "F3", LastName="L3"},
-         new Author {AuthorId = 4, FirstName = "F4", LastName="L4"},
-         new Author {AuthorId = 5, FirstName = "F5", LastName="L5"},
-};
+      {
+            new Author {AuthorId = 1, FirstName = "F1", LastName="L1"},
+            new Author {AuthorId = 2, FirstName = "F2", LastName="L2"},
+            new Author {AuthorId = 3, FirstName = "B_F3", LastName="b_L3"},
+            new Author {AuthorId = 4, FirstName = "F4", LastName="L4"},
+            new Author {AuthorId = 5, FirstName = "A_F5", LastName="a_L5"},
+      };
+
       [Test]
-      public void Index_ReturnsCorrectViewWithAllAuthors()
+      public void GetAuthors_ReturnsCorrectJsonResult()
       {
          //Arrange
          IRepository mockRepo = Substitute.For<IRepository>();
          mockRepo.Authors.Returns(_authors);
 
          //Act
-         AuthorController target = new AuthorController(mockRepo);
-         ActionResult result = target.Index();
+         AuthorsController target = new AuthorsController(mockRepo);
+         IActionResult result = target.GetAuthors();
+         int expectedCount = _authors.Count();
 
          //Assert
-         Assert.IsInstanceOf<ViewResult>(result);
-         IEnumerable<Author> model = (result as ViewResult).Model as IEnumerable<Author>;
-         Assert.AreEqual(_authors.Count(), model.Count());
-         Assert.AreEqual("L1", model.First().LastName);
+         Assert.IsInstanceOf<JsonResult>(result);
+         dynamic json = new DynamicWrapper(result);
+         Assert.AreEqual(expectedCount, json.Count);
+         Assert.AreEqual(5, json[0].AuthorId);
+         Assert.AreEqual(3, json[1].AuthorId);
+         Assert.AreEqual(1, json[2].AuthorId);
+         Assert.AreEqual(2, json[3].AuthorId);
+         Assert.AreEqual(4, json[4].AuthorId);
       }
 
       [Test]
-      public void Create_GET_ReturnsCorrectView()
-      {
-         //Arrange
-         IRepository mockRepo = Substitute.For<IRepository>();
-
-         //Act
-         AuthorController target = new AuthorController(mockRepo);
-         ActionResult result = target.Create();
-
-         //Assert
-         Assert.IsInstanceOf<ViewResult>(result);
-      }
-
-      [Test]
-      public void Create_POST_InvalidModel_ReturnsCorrectView()
+      public void GetAuthor_IncorrectId_ReturnsNotFound()
       {
          //Arrange
          IRepository mockRepo = Substitute.For<IRepository>();
          mockRepo.Authors.Returns(_authors);
-
+         int incorrectId = -1;
          //Act
-         Author invalidModel = new Author();
-         AuthorController target = new AuthorController(mockRepo);
-         ActionResult result = target.Create(invalidModel);
-
-         //Assert
-         Assert.IsInstanceOf<ViewResult>(result);
-         Author model = (result as ViewResult).Model as Author;
-         Assert.AreEqual(invalidModel, model);
-      }
-
-      [Test]
-      public void Create_POST_ValidModel_ReturnsCorrectView()
-      {
-         //Arrange
-         IRepository mockRepo = Substitute.For<IRepository>();
-         Author validModel = new Author { FirstName = "FirstName", LastName = "LastName" };
-         mockRepo.Authors.Returns(_authors);
-
-         //Act
-         AuthorController target = new AuthorController(mockRepo);
-         ActionResult result = target.Create(validModel);
-
-         //Assert
-         mockRepo.Received(1).AddAuthor(Arg.Any<Author>());
-         mockRepo.Received(1).SaveChanges();
-         Assert.IsInstanceOf<RedirectToActionResult>(result);
-         Assert.AreEqual("Index", (result as RedirectToActionResult).ActionName);
-      }
-
-      [Test]
-      public void Edit_GET_InvalidId_ReturnsHttpNotFound()
-      {
-         //Arrange
-         IRepository mockRepo = Substitute.For<IRepository>();
-         int invalidid = 999;
-         mockRepo.Authors.Returns(_authors);
-
-         //Act
-         AuthorController target = new AuthorController(mockRepo);
-         ActionResult result = target.Edit(invalidid);
+         AuthorsController target = new AuthorsController(mockRepo);
+         IActionResult result = target.GetAuthor(incorrectId);
 
          //Assert
          Assert.IsInstanceOf<NotFoundResult>(result);
       }
 
       [Test]
-      public void Edit_GET_ValidId_ReturnsCorrectView()
+      public void GetAuthor_CorrectId_ReturnsCorrectJsonResult()
       {
          //Arrange
          IRepository mockRepo = Substitute.For<IRepository>();
-         int validid = 1;
          mockRepo.Authors.Returns(_authors);
-         Author expected = _authors.Where(a => a.AuthorId == validid).First();
+         int incorrectId = 1;
          //Act
-         AuthorController target = new AuthorController(mockRepo);
-         ActionResult result = target.Edit(validid);
+         AuthorsController target = new AuthorsController(mockRepo);
+         IActionResult result = target.GetAuthor(incorrectId);
 
          //Assert
-         Assert.IsInstanceOf<ViewResult>(result);
-         Author model = (result as ViewResult).Model as Author;
-         Assert.AreEqual(expected, model);
+         Assert.IsInstanceOf<JsonResult>(result);
+         dynamic json = new DynamicWrapper(result);
+         Assert.AreEqual(1, json.AuthorId);
       }
 
       [Test]
-      public void Edit_POST_InvalidModel_ReturnsCorrectView()
+      public void PutAuthor_InvalidModel_ReturnsJsonResultWithErrors()
       {
          //Arrange
          IRepository mockRepo = Substitute.For<IRepository>();
          mockRepo.Authors.Returns(_authors);
-
+         CreateEditAuthorViewModel invalidModel = new CreateEditAuthorViewModel();
          //Act
-         Author invalidModel = new Author();
-         AuthorController target = new AuthorController(mockRepo);
-         target.ModelState.AddModelError("error", "some error");
-         ActionResult result = target.Edit(invalidModel);
-
+         AuthorsController target = new AuthorsController(mockRepo);
+         Task<IActionResult> task = target.PutAuthor(invalidModel);
+         IActionResult result = task.Result;
          //Assert
-         Assert.IsInstanceOf<ViewResult>(result);
-         Author model = (result as ViewResult).Model as Author;
-         Assert.AreEqual(invalidModel, model);
+         Assert.IsInstanceOf<JsonResult>(result);
+         dynamic json = new DynamicWrapper(result);
+         string errors = json.errors;
+         Assert.IsTrue(errors.Contains("Неверный идентификатор"));
+         Assert.IsTrue(errors.Contains("Не указано имя автора"));
+         Assert.IsTrue(errors.Contains("Не указана фамилия автора"));
       }
 
       [Test]
-      public void Edit_POST_ValidModelWithValidId_ReturnsCorrectView()
+      public void PutAuthor_ValidModel_InvalidId_ReturnsNotFound()
       {
          //Arrange
          IRepository mockRepo = Substitute.For<IRepository>();
          mockRepo.Authors.Returns(_authors);
-         int valiId = 1;
-         Author validModel = new Author { AuthorId = valiId, FirstName = "FirstName", LastName = "LastName" };
-         Author repoModel = _authors.Where(a => a.AuthorId == valiId).First();
+         CreateEditAuthorViewModel invalidModel = new CreateEditAuthorViewModel { AuthorId = 999, FirstName = "SomeFirstName", LastName = "SomeLastName" };
+         //Act
+         AuthorsController target = new AuthorsController(mockRepo);
+         Task<IActionResult> task = target.PutAuthor(invalidModel);
+         IActionResult result = task.Result;
+         //Assert
+         Assert.IsInstanceOf<BadRequestResult>(result);
+      }
+
+      [Test]
+      public void PutAuthor_ValidModel_ValidId_ReturnsOkObjectResult()
+      {
+         //Arrange
+         IRepository mockRepo = Substitute.For<IRepository>();
+         mockRepo.Authors.Returns(_authors);
+         CreateEditAuthorViewModel validModel = new CreateEditAuthorViewModel { AuthorId = 1, FirstName = "SomeFirstName", LastName = "SomeLastName" };
+         Author repoModel = _authors.Where(a => a.AuthorId == validModel.AuthorId).First();
 
          //Act
-         AuthorController target = new AuthorController(mockRepo);
-         ActionResult result = target.Edit(validModel);
+         AuthorsController target = new AuthorsController(mockRepo);
+         Task<IActionResult> task = target.PutAuthor(validModel);
+         IActionResult result = task.Result;
 
          //Assert
-         mockRepo.Received(1).SaveChanges();
+         mockRepo.Received(1).SaveChangesAsync();
          Assert.AreEqual(validModel.FirstName, repoModel.FirstName);
          Assert.AreEqual(validModel.LastName, repoModel.LastName);
-         Assert.IsInstanceOf<RedirectToActionResult>(result);
-         Assert.AreEqual("Index", (result as RedirectToActionResult).ActionName);
+         Assert.IsInstanceOf<OkObjectResult>(result);
+         dynamic json = new DynamicWrapper(result);
+         Assert.AreEqual(true, json.success);
       }
 
       [Test]
-      public void Edit_POST_ValidModelButInValidId_ReturnsHttpNotFound()
+      public void PutAuthor_ValidModel_ValidId_OnDbUpdateConcurrencyException_ReturnsOkObjectResult()
       {
          //Arrange
          IRepository mockRepo = Substitute.For<IRepository>();
          mockRepo.Authors.Returns(_authors);
-         int invaliId = 100;
-         Author validModel = new Author { AuthorId = invaliId, FirstName = "FirstName", LastName = "LastName" };
+         mockRepo.When((mock) => mock.SaveChangesAsync()).Do((mock) => throw new DbUpdateConcurrencyException());
+         CreateEditAuthorViewModel validModel = new CreateEditAuthorViewModel { AuthorId = 1, FirstName = "SomeFirstName", LastName = "SomeLastName" };
+         Author repoModel = _authors.Where(a => a.AuthorId == validModel.AuthorId).First();
 
          //Act
-         AuthorController target = new AuthorController(mockRepo);
-         ActionResult result = target.Edit(validModel);
+         AuthorsController target = new AuthorsController(mockRepo);
+         Task<IActionResult> task = target.PutAuthor(validModel);
+         IActionResult result = task.Result;
 
+         //Assert
+         mockRepo.Received(1).SaveChangesAsync();
+         Assert.IsInstanceOf<JsonResult>(result);
+         dynamic json = new DynamicWrapper(result);
+         string errors = json.errors;
+         Assert.IsTrue(errors.Contains("Данные были оновлены"));
+      }
+
+      [Test]
+      public void PostAuthor_InvalidModel_ReturnsJsonResultWithErrors()
+      {
+         //Arrange
+         IRepository mockRepo = Substitute.For<IRepository>();
+         mockRepo.Authors.Returns(_authors);
+         CreateEditAuthorViewModel invalidModel = new CreateEditAuthorViewModel();
+         //Act
+         AuthorsController target = new AuthorsController(mockRepo);
+         Task<IActionResult> task = target.PostAuthor(invalidModel);
+         IActionResult result = task.Result;
+         //Assert
+         Assert.IsInstanceOf<JsonResult>(result);
+         dynamic json = new DynamicWrapper(result);
+         string errors = json.errors;
+         Assert.IsTrue(errors.Contains("Не указано имя автора"));
+         Assert.IsTrue(errors.Contains("Не указана фамилия автора"));
+      }
+
+      [Test]
+      public void PostAuthor_ValidModel_ValidId_ReturnsOkObjectResult()
+      {
+         //Arrange
+         IRepository mockRepo = Substitute.For<IRepository>();
+         mockRepo.Authors.Returns(_authors);
+         CreateEditAuthorViewModel validModel = new CreateEditAuthorViewModel { FirstName = "SomeFirstName", LastName = "SomeLastName" };
+         //Act
+         AuthorsController target = new AuthorsController(mockRepo);
+         Task<IActionResult> task = target.PostAuthor(validModel);
+         IActionResult result = task.Result;
+         //Assert
+         mockRepo.Received(1).AddAuthor(Arg.Any<Author>());
+         mockRepo.Received(1).SaveChangesAsync();
+         Assert.IsInstanceOf<OkObjectResult>(result);
+         dynamic json = new DynamicWrapper(result);
+         Assert.AreEqual(true, json.success);
+      }
+
+      [Test]
+      public void DeleteAuthor_IncorrectId_ReturnsNotFound()
+      {
+         //Arrange
+         IRepository mockRepo = Substitute.For<IRepository>();
+         mockRepo.Authors.Returns(_authors);
+         int incorrectId = -1;
+         //Act
+         AuthorsController target = new AuthorsController(mockRepo);
+         IActionResult result = target.DeleteAuthor(incorrectId).Result;
          //Assert
          Assert.IsInstanceOf<NotFoundResult>(result);
       }
 
       [Test]
-      public void Delete_GET_InvalidId_ReturnsHttpNotFound()
-      {
-         //Arrange
-         IRepository mockRepo = Substitute.For<IRepository>();
-         int invalidid = 999;
-         mockRepo.Authors.Returns(_authors);
-
-         //Act
-         AuthorController target = new AuthorController(mockRepo);
-         ActionResult result = target.Delete(invalidid);
-
-         //Assert
-         Assert.IsInstanceOf<NotFoundResult>(result);
-      }
-
-      [Test]
-      public void Delete_GET_ValidId_ReturnsCorrectView()
-      {
-         //Arrange
-         IRepository mockRepo = Substitute.For<IRepository>();
-         int validid = 1;
-         mockRepo.Authors.Returns(_authors);
-         Author expected = _authors.Where(a => a.AuthorId == validid).First();
-         //Act
-         AuthorController target = new AuthorController(mockRepo);
-         ActionResult result = target.Delete(validid);
-
-         //Assert
-         Assert.IsInstanceOf<ViewResult>(result);
-         Author model = (result as ViewResult).Model as Author;
-         Assert.AreEqual(expected, model);
-      }
-
-      [Test]
-      public void DeleteConfirmed_InvalidId_ReturnsHttpNotFound()
+      public void DeleteAuthor_CorrectId_ReturnsCorrectOkObjectResult()
       {
          //Arrange
          IRepository mockRepo = Substitute.For<IRepository>();
          mockRepo.Authors.Returns(_authors);
-
+         int correctId = 1;
          //Act
-         int invalidId = 999;
-         AuthorController target = new AuthorController(mockRepo);
-         ActionResult result = target.DeleteConfirmed(invalidId);
-
-         //Assert
-         Assert.IsInstanceOf<NotFoundResult>(result);
-      }
-
-      [Test]
-      public void DeleteConfirmed_ValidId_ReturnsCorrectView()
-      {
-         //Arrange
-         IRepository mockRepo = Substitute.For<IRepository>();
-         mockRepo.Authors.Returns(_authors);
-
-         //Act
-         int validId = 1;
-         AuthorController target = new AuthorController(mockRepo);
-         ActionResult result = target.DeleteConfirmed(validId);
-
+         AuthorsController target = new AuthorsController(mockRepo);
+         IActionResult result = target.DeleteAuthor(correctId).Result;
          //Assert
          mockRepo.Received(1).RemoveAuthor(Arg.Any<Author>());
-         mockRepo.Received(1).SaveChanges();
-         Assert.IsInstanceOf<RedirectToActionResult>(result);
-         Assert.AreEqual("Index", (result as RedirectToActionResult).ActionName);
+         mockRepo.Received(1).SaveChangesAsync();
+         Assert.IsInstanceOf<OkObjectResult>(result);
       }
-
    }
 }
